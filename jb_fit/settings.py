af5e2990e86e3c3d1e99b289c3c1c1b8c5ad2d1e
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-08-07 13:29:23 +0100

    feat: add home page mockups to readme

--- jb_fit/settings.py
+++ jb_fit/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iylnd)g**p@8mq--*dtdng2izr3i=nu!jf@k8-ye@yd3s17$oz'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://ci-jb-fit-73ac55dce174.herokuapp.com/']
 
 
 # Application definition
