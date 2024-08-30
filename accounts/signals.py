6a597456ba98a2e5c2aeda20e84dbcd1529868dc
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-08-30 19:48:27 +0100

    feat: improve sign in message

---
+++ accounts/signals.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.contrib import messages
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, request, user, **kwargs):
+    # Use the first name of the user in the success message
+    first_name = user.first_name  # Fallback to username if first name is empty
+    messages.success(request, f"Welcome back, {first_name}!")
