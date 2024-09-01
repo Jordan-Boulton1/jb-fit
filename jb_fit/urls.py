4434b262dda05f3e7c43c6140178566a26fb2d19
Jordan <127216241+Jordan-Boulton1@users.noreply.github.com>
2024-09-01 01:46:09 +0100

    feat: add 404 page

--- jb_fit/urls.py
+++ jb_fit/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,3 +37,6 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+
+
+handler404 = 'jb_fit.views.handler404'
